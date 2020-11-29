87374d54eb5d700c04f0137264dcc775770f2724
Amanda Ciobanu <amanda@ciobanu.org>
2020-11-29 17:08:34 +0000

    chore: build to production

---
+++ build/precache-manifest.8194efdb169ae156446d370d33c66923.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8dc37f5886d04b314439119ca8f1aff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "46e3f6674e760cda18d2",
+    "url": "/static/css/main.0a0f4602.chunk.css"
+  },
+  {
+    "revision": "cf6bca8e816ecf72b2d4",
+    "url": "/static/js/2.11f691c1.chunk.js"
+  },
+  {
+    "revision": "1dbab337b90e8f2eb0a4a79d2378f49b",
+    "url": "/static/js/2.11f691c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46e3f6674e760cda18d2",
+    "url": "/static/js/main.9e86e9b2.chunk.js"
+  },
+  {
+    "revision": "7402b3a51c6a0325ccf5",
+    "url": "/static/js/runtime-main.297f6f71.js"
+  }
+]);
