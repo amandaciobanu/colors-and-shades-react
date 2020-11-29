d92ac08f766bbb4aafbd5bdf85d9b5fec2b3bbaf
Amanda Ciobanu <amanda@ciobanu.org>
2020-11-29 11:36:50 +0000

    feat: iterate over a single color

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ function App() {
     e.preventDefault();
     try {
       let colors = new Values(color).all(10)
+      setList(colors)
     }catch (error){
       setError(true)
     }
@@ -32,7 +33,11 @@ function App() {
         </form>
       </section>
       <section className="colors">
-        <h4>List goes here</h4>
+        {list.map((color, index) => {
+
+          return <SingleColor key={index} {...color} index={index}/>
+
+        })}
       </section>
       </>
   )
