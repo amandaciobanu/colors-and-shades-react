99742898ad7d38a07586c31289997c842b931455
Amanda Ciobanu <amanda@ciobanu.org>
2020-11-29 17:04:39 +0000

    feat: new message to user/colors

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Values from 'values.js'
 function App() {
   const [color, setColor] = useState('');
   const [error, setError] = useState(false);
-  const [list, setList] = useState([]);
+  const [list, setList] = useState(new Values ('#EAA4A4').all(10));
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -21,7 +21,7 @@ function App() {
   return (
       <>
       <section className='container'>
-        <h3>Color Generator</h3>
+        <h3>Shades & Colors Generator</h3>
         <form onSubmit={handleSubmit}>
           <input type="text" value={color}
                   onChange={(e) =>
@@ -32,6 +32,9 @@ function App() {
           <button className='btn' type='submit'>Submit</button>
         </form>
       </section>
+        <section className='text-user'>
+          <p><small>Choose your favorite color and copy from the clipboard</small></p>
+        </section>
       <section className="colors">
         {list.map((color, index) => {
           return <SingleColor key={index} {...color} index={index}
