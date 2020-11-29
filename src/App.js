ff702a080cdcd5724b2bf68be7d7bc4bd9689096
Amanda Ciobanu <amanda@ciobanu.org>
2020-11-29 11:17:29 +0000

    feat: form submit color

--- src/App.js
+++ src/App.js
@@ -4,7 +4,33 @@ import SingleColor from './SingleColor'
 import Values from 'values.js'
 
 function App() {
-  return <h2>color generator project</h2>
+  const [color, setColor] = useState('');
+  const [error, setError] = useState(false);
+  const [list, setList] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('hello')
+  }
+
+  return (
+      <>
+      <section className='container'>
+        <h3>Color Picker</h3>
+        <form onSubmit={handleSubmit}>
+          <input type="text" value={color}
+                  onChange={(e) =>
+                  setColor(e.target.value)}
+                  placeholder='#EAA4A4'
+          />
+          <button className='btn' type='submit'>Submit</button>
+        </form>
+      </section>
+      <section className="colors">
+        <h4>List goes here</h4>
+      </section>
+      </>
+  )
 }
 
 export default App
