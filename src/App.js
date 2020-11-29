0b13bda0c768503ceca4fc1164f3c93046692125
Amanda Ciobanu <amanda@ciobanu.org>
2020-11-29 16:22:34 +0000

    feat: function writeText to copy color from clipboard

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
   return (
       <>
       <section className='container'>
-        <h3>Color Picker</h3>
+        <h3>Color Generator</h3>
         <form onSubmit={handleSubmit}>
           <input type="text" value={color}
                   onChange={(e) =>
